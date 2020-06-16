c477580048b857111ba365eea57a02f17085a1f4
rstyczynski <rstyczynski@gmail.com>
2020-06-16 18:59:19 +0200

    trace newly discovered events

---
+++ bin/mft_trace_events.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mft_tracer=$1
+logfile=$2
+while IFS= read -r event_id; do
+  echo $event_id
+  curl -s "http://$mft_tracer/mft/trace?eventId=$event_id"
+done <<<$(tail -F $logfile | grep -Po '(?<=jca.jms.JMSProperty.EventSessionID=)[\w-]+')
