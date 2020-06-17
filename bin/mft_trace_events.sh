b55ad936368369ccf5a9275b5dd72e3f09c5a4a0
rstyczynski <rstyczynski@gmail.com>
2020-06-17 11:31:40 +0200

    .

--- bin/mft_trace_events.sh
+++ bin/mft_trace_events.sh
@@ -5,3 +5,5 @@ while IFS= read -r event_id; do
   echo $event_id
   curl -s "http://$mft_tracer/mft/trace?eventId=$event_id"
 done <<<$(tail -F $logfile | grep -Po '(?<=jca.jms.JMSProperty.EventSessionID=)[\w-]+')
+
+
